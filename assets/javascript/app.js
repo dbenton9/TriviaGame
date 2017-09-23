51b071edae5025cc21ba5fd8abc2bd33c86494e2
David Benton <soccershelbyb15@gmail.com>
2017-09-22 22:40:33 -0400

    rough draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,52 @@
 //================================================================================
 
 // objects
-Q1 = {
-	question: "What is another name for Gandalf?",
-	answerArr: ['A', 'B','C','D'],
-	solution: 'A'
-};
+myQuestions = [
+	{
+		question: "What is not a name for Gandalf?",
+		answerArr: {
+			A: 'A. Mithrandir', 
+			B: 'B. Stormcrow',
+			C: 'C. Tharkun',
+			D: 'D. Diedres'
+			},
+		solution: 'D'
+	},
+	{
+		question: "What are orcs?",
+		answerArr: {
+			A: 'A. Monstors', 
+			B: 'B. Demons',
+			C: 'C. Elves',
+			D: 'D. Your mother in-law'
+			},
+		solution: 'C'
+	},
+	{
+		question: "Where is Mordor?",
+		answerArr: {
+			A: 'A. Mordor', 
+			B: 'B. The Shire',
+			C: 'C. Gondor',
+			D: 'D. Rohan'
+			},
+		solution: 'A'
+	}
+]
+
+// what is not a name for gandalf?
+console.log(myQuestions[0].question);
+// elvs
+console.log(myQuestions[1].answerArr.C);
 
 // variables
-var num = 30;
+
+// timer
+var num;
 var intervalId;
+
+//question counter
+var counter = 0;
 
 var audio = new Audio("alarm.m4a");
 
@@ -22,13 +59,13 @@ var audio = new Audio("alarm.m4a");
 
 // starts trivia
 $("#start").on("click", function(){
-	run();
+	//run();
 	triviaStart();
 });
 
-//takes user guess
+// takes user guess
 $(".guess").on("click", function(){
-	audio.play();
+	checkGuess();
 });
 
 
@@ -36,17 +73,36 @@ $(".guess").on("click", function(){
 // game functions
 //================================================================================
 
-// RUN, DECREASE, and STOP functions for countdown timer
-function run(){
+// setInterval, DECREASE, and STOP functions for countdown timer
+// [function run(){
+// 	intervalId = setInterval(decrease, 1000);
+// }]
+
+// triviaStart populates fields at start
+function triviaStart(){
+	
+	num = 10;
 	intervalId = setInterval(decrease, 1000);
-}
+
+	$("#prompt").html(myQuestions[counter].question);
+	$("#ans0").html(myQuestions[counter].answerArr.A);
+	$("#ans1").html(myQuestions[counter].answerArr.B);
+	$("#ans2").html(myQuestions[counter].answerArr.C);
+	$("#ans3").html(myQuestions[counter].answerArr.D);
+	counter++;
+	$("#question").html("Question " + counter);
+
+	if (counter === myQuestions.length){
+		stop();
+	}
+};
 
 function decrease(){
 	$("#timer").html("<h2>" + num + "</h2>");
 	num--;
-
-	if (num <= 0) {
-		stop();
+	console.log(num);
+	if (num < 0) {
+		triviaStart();
 	}
 }
 
@@ -54,16 +110,13 @@ function stop(){
 	clearInterval(intervalId);
 }
 
+// checking the guess
+function checkGuess(){
 
-// populates fields at start
-function triviaStart(){
-	$("#question").html("Question 1")
-	$("#prompt").html(Q1.question)
-	$("#ans0").html(Q1.answerArr[0])
-	$("#ans1").html(Q1.answerArr[1])
-	$("#ans2").html(Q1.answerArr[2])
-	$("#ans3").html(Q1.answerArr[3])
-};
+}
+
+
+
 
 
 
